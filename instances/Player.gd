17d05917ca6a1eb3eb5494eb2a08d7101d96bca8
Asta <jeremiefivat@hotmail.com>
2018-02-04 10:47:56 +0100

    map + hero movement

---
+++ instances/Player.gd
@@ -0,0 +1,93 @@
+extends Area2D
+
+var i_pos = 0
+var j_pos = 0
+var i_pos_new = 0
+var j_pos_new = 0
+var player_num = 0
+var ini = false
+var player_control = true
+var death = false
+var move_input = "none"
+const MOVE_DELAY = .2
+var move_delay = MOVE_DELAY
+
+func _ready():
+	_position_ini()
+	pass
+
+func _process(delta):
+	move_delay -= delta
+	if move_delay < 0:
+		player_control = true
+		move_delay = MOVE_DELAY
+		
+	if ini == false:
+		_hero_ini()
+		ini = true
+		
+	if not move_input == "none":
+		if player_control == true:
+			if move_input == "left":
+				if i_pos > 0:
+					if GameManager.array_map_obst[i_pos-1][j_pos] == 0:
+						i_pos_new = i_pos-1
+			if move_input == "right":
+				if i_pos < GameManager.tile_i_max-1:
+					if GameManager.array_map_obst[i_pos+1][j_pos] == 0:
+						i_pos_new = i_pos+1
+			if move_input == "top":
+				if j_pos > 0:
+					if GameManager.array_map_obst[i_pos][j_pos-1] == 0:
+						j_pos_new = j_pos -1
+			if move_input == "down":
+				if j_pos < GameManager.tile_j_max-1:
+					if GameManager.array_map_obst[i_pos][j_pos+1] == 0:
+						j_pos_new = j_pos+1
+			if not i_pos == i_pos_new or not j_pos == j_pos_new:
+				player_control = false
+				
+		move_input = "none"
+	
+	if not i_pos_new == i_pos or not j_pos_new == j_pos:
+		#free the slot
+		GameManager.array_map_obst[i_pos][j_pos] = 0
+		i_pos = i_pos_new
+		j_pos = j_pos_new
+		#fill the slot
+		GameManager.array_map_obst[i_pos][j_pos] = 1
+		_position_ini()
+	pass
+	
+func _input(event):
+	move_input = "none"
+	#controls
+	if event.is_action("ui_left"):
+		move_input = "left"
+	if event.is_action("ui_right"):
+		move_input = "right"
+	if event.is_action("ui_up"):
+		move_input = "top"
+	if event.is_action("ui_down"):
+		move_input = "down"
+		
+
+func _position_ini():
+	position.x = i_pos * GameManager.tile_size+GameManager.dungeon_left_margin + GameManager.tile_size/2
+	position.y = j_pos * GameManager.tile_size+GameManager.dungeon_top_margin + GameManager.tile_size/6
+	pass
+	
+func _hero_ini():
+	var hero_sprite = "res://sprites/heroes/hero01.png"
+	if player_num == 0:
+		hero_sprite = "res://sprites/heroes/hero01.png"
+	if player_num == 1:
+		hero_sprite = "res://sprites/heroes/hero02.png"
+	if player_num == 2:
+		hero_sprite = "res://sprites/heroes/hero03.png"
+	if player_num == 3:
+		hero_sprite = "res://sprites/heroes/hero04.png"
+		
+	$Player_Sprite.texture = load(hero_sprite)
+	pass
+	
--- scripts/game_manager.gd
+++ scripts/game_manager.gd
@@ -1,4 +1,15 @@
 extends Node
+#i size of the map
+var tile_i_max = 20
+#j size of the map
+var tile_j_max = 12
+#tile size in pixels
+var tile_size = 64
+# map ...
+var array_map_tile = []
+var array_map_obst = []
+var dungeon_left_margin = 100
+var dungeon_top_margin = 50
 
 # Default game port
 const DEFAULT_PORT = 10567
@@ -13,6 +24,17 @@ var player_name
 var players = {}
 
 func _ready():
+	#initialize map
+	var i
+	var j
+	array_map_tile = create_map(tile_i_max,tile_j_max)
+	array_map_obst = create_map(tile_i_max,tile_j_max)
+	for i in range(tile_i_max):
+		for j in range(tile_j_max):
+			array_map_tile[i][j] = "floor_stone"
+			array_map_obst[i][j] = 0
+	
+	
 	get_tree().connect("network_peer_connected", self, "on_player_connected")
 
 func on_player_connected(player_id):
@@ -31,3 +53,13 @@ func join_game(ip, new_player_name):
 	var peer = NetworkedMultiplayerENet.new()
 	peer.create_client(ip, DEFAULT_PORT)
 	get_tree().set_network_peer(peer)
+	
+func create_map(w, h):
+	var map = []
+	
+	for x in range(w):
+		var col = []
+		col.resize(h)
+		map.append(col)
+	
+	return map
